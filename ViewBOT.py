1885dbb5a7a60435fd31d9725158db18a5c2eb02
YANKvp <37215794+YANKvp@users.noreply.github.com>
2018-03-12 12:21:24 +0000

    Update ViewBOT.py

--- ViewBOT.py
+++ ViewBOT.py
@@ -1,4 +1,6 @@
+#Autor-YANK @2018ViewPoint
 
+#import 
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
@@ -8,26 +10,28 @@ import logging
 import random
 import chalk
 
+#nadajemy prefix jaki jest potrzebny do dzialania komendy.
 bot=commands.Bot(command_prefix=">")
 
-
+#info do debugu 
 @bot.event
 async def on_ready():
     print("Bot zostal podlaczony pod ViewPointSerwer!")
 
-
+#komendy(przyklad)
 @bot.command()
-async def test():
-    await bot.say("test")
-@bot.command()
-async def dupa():
-    await bot.say("dupa")
+async def komendajakas():
+    await bot.say("tutaj np link do filmiku Youtube")
 
+
+#Po prostu echo
 @bot.command (pass_context = True)
 async def echo (ctx, *, echo:str):
     await bot.delete_message(ctx.message)
     await bot.say (" echo: " + echo)
 
+
+#Dashboard uzytkownikow	
 @bot.command(pass_context=True)
 async def info(ctx, user: discord.Member):
     embed = discord.Embed(title="{}Dashboard użytkownika".format(user.name), description="Oto Znalezione informacje.", color=0x00ff00)
@@ -38,7 +42,8 @@ async def info(ctx, user: discord.Member):
     embed.set_thumbnail(url=user.avatar_url)
     await bot.say(embed=embed)
 
-    
+  
+#system piw
 @bot.command(pass_context=True)
 async def piwo(ctx, *, member : discord.Member = None):
     if member is None:
@@ -49,12 +54,14 @@ async def piwo(ctx, *, member : discord.Member = None):
         else:
             await bot.say(ctx.message.author.mention + " postawił piwo dla" + member.mention +":beer:")
 
+#system wybierania przez bota wpisanych przez uzytkownika tresci
 @bot.command(pass_context = True)
 async def wybierz(ctx, *, choices: str):
     takisom = choices.split(",")
     chosen= takisom[random.randrange(len(takisom))]
     await bot.say(ctx.message.author.mention + ": Wybieram `" + chosen + "`")
 
+#kick
 @bot.command(pass_context=True)
 async def kick(ctx, user: discord.Member):
     await bot.say(":boot: {}Wyleciał z Serwera niczym kruk!!".format(user.name))
@@ -62,7 +69,8 @@ async def kick(ctx, user: discord.Member):
 
 
 
-   
-	 
+#podpinanie tokena bota  
 bot.run ("---")
 
+
+#troche syfowo ale caly czas ulepszam (nie wklejalem calego kodu jak cus, bot jest bardziej rozbudowany)
